f837957d317698018c75bc32ddc80b219e37fec5
Ashley <Ashley>
2026-09-09 21:43:29 -0300

    feat: adicionado a rota para acessar o caminho monta-tela-resumo com o caminho resumo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { Home } from './components/home/home';
 import { Carrinho } from './components/carrinho/carrinho';
+import { MontaResumoCompra } from './monta-resumo-compra/monta-resumo-compra';
 
 export const routes: Routes = [
     {path: '', redirectTo: 'home', pathMatch: 'full'},
     {path: 'home', component: Home},
+    { path: 'resumo', component: MontaResumoCompra },
     {path: 'carrinho', component: Carrinho}
 ];

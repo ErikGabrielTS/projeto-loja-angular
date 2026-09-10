89e40a14e493f10574aefd6f2c32edd89e4d9ef9
Ashley <Ashley>
2026-09-09 21:40:01 -0300

    feat: adicioando as funções de cep no ts de monta-resumo-compra

--- src/app/monta-resumo-compra/monta-resumo-compra.ts
+++ src/app/monta-resumo-compra/monta-resumo-compra.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CarrinhoService } from '../../services/carrinho-service';
 
 @Component({
   selector: 'app-monta-resumo-compra',
@@ -6,4 +7,54 @@ import { Component } from '@angular/core';
   templateUrl: './monta-resumo-compra.html',
   styleUrl: './monta-resumo-compra.css',
 })
-export class MontaResumoCompra {}
+export class MontaResumoCompra implements OnInit {
+  cep: string = '';
+  valorFrete: number = 0;
+  freteCalculado: boolean = false;
+  carregandoFrete: boolean = false;
+  erroFrete: string = '';
+  cidadeEstado: string = '';
+
+  constructor(public carrinhoService: CarrinhoService) {}
+
+  ngOnInit(): void {}
+
+  calcularFrete(): void {
+    const cepLimpo = this.cep.replace(/\D/g, '');
+
+    if (cepLimpo.length !== 8) {
+      this.erroFrete = 'Digite um CEP válido com 8 dígitos.';
+      this.freteCalculado = false;
+      return;
+    }
+
+    this.carregandoFrete = true;
+    this.erroFrete = '';
+
+    this.carrinhoService.consultarFreteAPI(cepLimpo).subscribe({
+      next: (dados) => {
+        this.carregandoFrete = false;
+        if (dados.erro) {
+          this.erroFrete = 'CEP não encontrado.';
+          this.freteCalculado = false;
+          this.cidadeEstado = '';
+        } else {
+          // Exemplo: Define valor fixo de frete e exibe a localização
+          this.valorFrete = 15.00;
+          this.freteCalculado = true;
+          this.cidadeEstado = `${dados.localidade} - ${dados.uf}`;
+        }
+      },
+      error: () => {
+        this.carregandoFrete = false;
+        this.erroFrete = 'Erro ao consultar o CEP.';
+        this.freteCalculado = false;
+      }
+    });
+  }
+
+  obterTotalFinal(): number {
+    return this.carrinhoService.obterSubtotal() + (this.freteCalculado ? this.valorFrete : 0);
+  }
+}
+
